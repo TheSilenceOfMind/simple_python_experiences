6fa8c1d4959351b94fc1af7a33cf9bb98325c758
TheSilenceOfMind <baladkirill@mail.ru>
2018-02-09 23:42:59 +0300

    detached to a function the code related with writing the result to files

--- untitled/Tolstoy/text_analizer.py
+++ untitled/Tolstoy/text_analizer.py
@@ -5,23 +5,57 @@ from os import write
 # it's need to get access to top-directory files
 sys.path.append("..")
 from reading_files import get_words, get_files
+
 sys.path.pop()
 
 
 def get_clean_list(old_list):
-    '''
+    """
     The function removes all redudant characters from word to get the
     'clean' word
 
     :param old_list: list of 'dirty' words
     :return: list of 'clean' words
-    '''
+    """
     for idx, word in enumerate(old_list):
         word = str(word)
         upd_w = word.lower()
         upd_w = upd_w.strip(' .,!?:;"-«()[]\'')
         old_list[idx] = upd_w
     return old_list
+
+
+def write_statistics_to_file(f, total_amount_of_words, d):
+    """
+    The method is used to write all statistics to file :
+        1) count of words_entries
+        2) total amount of words
+
+    :param f: is filename - string
+    :param total_amount_of_words in dictionary
+    :param d: concrete dictionary
+    :return: nothing
+    """
+    out1 = open("output_" + f, 'w')
+    out1.write("total amount of words = " + str(total_amount_of_words) + "\n")
+    out2 = open("output_more_3_letters_" + f, 'w')
+    reduced_amount_of_words = 0
+    for key, value in sorted(d.items(), key=itemgetter(1), reverse=True):
+        out1.write(str(key))
+        if len(key) > 3:
+            out2.write(str(key))
+            whitesp = 40 - len(key)  # pretty-nice output
+            out2.write(' ' * whitesp)
+            out2.write(str(value))
+            out2.write('\n')
+            reduced_amount_of_words += value
+        whitesp = 40 - len(key)
+        out1.write(' ' * whitesp)
+        out1.write(str(value))
+        out1.write('\n')
+    out2.write(str(reduced_amount_of_words))
+    out2.close()
+    out1.close()
 
 
 # get all files to process
@@ -43,24 +77,4 @@ for f in files:
         # some online log
         print(str(cnt) + " / " + str(total_amount_of_words))
 
-    # write statistics to certain file
-    out1 = open("output_" + f, 'w')
-    out1.write("total amount of words = " + str(total_amount_of_words) + "\n")
-    out2 = open("output_more_3_letters_" + f, 'w')
-    reduced_amount_of_words = 0
-    for key, value in sorted(d.items(), key=itemgetter(1), reverse=True):
-        out1.write(str(key))
-        if len(key) > 3:
-            out2.write(str(key))
-            whitesp = 40 - len(key)  # pretty-nice output
-            out2.write(' ' * whitesp)
-            out2.write(str(value))
-            out2.write('\n')
-            reduced_amount_of_words += value
-        whitesp = 40-len(key)
-        out1.write(' ' * whitesp)
-        out1.write(str(value))
-        out1.write('\n')
-    out2.write(str(reduced_amount_of_words))
-    out2.close()
-    out1.close()
+    write_statistics_to_file(f, total_amount_of_words, d)
