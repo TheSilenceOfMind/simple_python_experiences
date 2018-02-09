d5f88702d22a0de5c067a0fe1ad2b751e7039f2d
TheSilenceOfMind <baladkirill@mail.ru>
2018-02-09 23:11:44 +0300

    added comments and little bit of documentation to code

--- untitled/Tolstoy/text_analizer.py
+++ untitled/Tolstoy/text_analizer.py
@@ -2,11 +2,20 @@ import sys
 from operator import itemgetter
 from os import write
 
+# it's need to get access to top-directory files
 sys.path.append("..")
 from reading_files import get_words, get_files
 sys.path.pop()
 
+
 def get_clean_list(old_list):
+    '''
+    The function removes all redudant characters from word to get the
+    'clean' word
+
+    :param old_list: list of 'dirty' words
+    :return: list of 'clean' words
+    '''
     for idx, word in enumerate(old_list):
         word = str(word)
         upd_w = word.lower()
@@ -15,13 +24,15 @@ def get_clean_list(old_list):
     return old_list
 
 
-files = get_files(path='.')
+# get all files to process
+path_to_files_to_read = '.'
+files = get_files(path=path_to_files_to_read)
 print(files)
-list_of_dict = []
+
+list_of_dict = []  # used to store all dictionaries for all files
 for f in files:
     d = {}
-    list_of_words = get_words(f)
-    list_of_words = get_clean_list(list_of_words)
+    list_of_words = get_clean_list(get_words(f))
     total_amount_of_words = len(list_of_words)
     set_of_words = set(list_of_words)
     cnt = 0
@@ -29,8 +40,11 @@ for f in files:
         amount = list_of_words.count(i)
         cnt += amount
         d[i] = amount
+        # some online log
         print(str(cnt) + " / " + str(total_amount_of_words))
     list_of_dict.append(d)
+
+    # write statistics to certain file
     out1 = open("output_" + f, 'w')
     out1.write("total amount of words = " + str(total_amount_of_words) + "\n")
     out2 = open("output_more_3_letters_" + f, 'w')
@@ -39,7 +53,7 @@ for f in files:
         out1.write(str(key))
         if len(key) > 3:
             out2.write(str(key))
-            whitesp = 40 - len(key)
+            whitesp = 40 - len(key)  # pretty-nice output
             out2.write(' ' * whitesp)
             out2.write(str(value))
             out2.write('\n')
